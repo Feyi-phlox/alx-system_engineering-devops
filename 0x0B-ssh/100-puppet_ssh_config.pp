117178676b2252748345de2429f7d8fe2463e818
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-09-28 04:28:03 -0700

    puppet fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,13 @@
-# Define the SSH client configuration
+# setting  up your client SSH configuration file
+# so that you can connect to a server without typing a password
 
-file { '~/.ssh/config':
-  ensure  => present,
-  content => "\
-Host 100.25.191.149
-	PasswordAuthentication no
-	IdentityFile ~/.ssh/school
-",
+file_line { 'turn password auth off':
+  ensure => present,
+  path   => 'etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+file_line { 'declare identity file':
+  ensure => present,
+  path   => 'etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
