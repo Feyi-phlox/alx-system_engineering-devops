d08301f0ce12ba4d5590e0180782bfc271c0a473
Feyi-phlox <feyisayoakinbobola@gmail.com>
2024-08-30 15:31:06 +0000

    some fix, hope it works this time

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """this queries the Reddit API and returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "myredditscript/0.1 (by u/feyi-phlox)"}
+    headers = {"User-Agent": "linux:alx.api.project:v1.0.0 (by /u/feyi-phlox)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
